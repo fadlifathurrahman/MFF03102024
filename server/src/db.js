cc2dfa3ae4fc3b53679d42ca94cb4462fcfd5a47
Muhammad Fadli Fathurrahman <fadlifathurrahmanm@gmail.com>
2024-10-08 10:49:39 +0700

    reconfigure database connection

--- server/src/db.js
+++ server/src/db.js
@@ -1,13 +1,21 @@
 import mariadb from 'mariadb';
 import "dotenv/config";
 
+// const pool = mariadb.createPool({
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_NAME,
+// });
+
 const pool = mariadb.createPool({
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-});
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "sekolah",
+  });
 
 const conn = await pool.getConnection();
 console.log('Connected to database');
